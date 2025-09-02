cb3b76dfc2e684ae2f8086b2853686306a43c2ab
admin <admin@DESKTOP-E07VVT6>
2025-09-02 16:24:01 +0500

    Разработанный дизайн главного окна. v1

---
+++ TV/Windows/Main.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TV.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainSettings.xaml
+    /// </summary>
+    public partial class MainSettings : Window
+    {
+        public MainSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
